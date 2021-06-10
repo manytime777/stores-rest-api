7b87c1e0c97f2ac0fd6fbc96fc6cd5281a19e94a
manytime777 <manytime@777.so-net.jp>
2021-06-10 23:08:37 +0900

    change app.py app.config debug

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from resources.store import Store, StoreList
 
 app = Flask(__name__)
 
-app.config['DEBUG'] = True
+#app.config['DEBUG'] = True
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -31,9 +31,9 @@ if __name__ == '__main__':
     from db import db
     db.init_app(app)
 
-    if app.config['DEBUG']:
-        @app.before_first_request
-        def create_tables():
-            db.create_all()
+#    if app.config['DEBUG']:
+#        @app.before_first_request
+#        def create_tables():
+#            db.create_all()
 
     app.run(port=5000)
